9833088b47c8b3cc19d838c35256cda2961cbed9
Luiz-Guilherme2907 <luizguilhermemodler2907@gmail.com>
2024-03-03 23:26:39 -0300

    creating the new exchange microservice, to convert amounts from dollars to reais and from dollars to other currencies. I implemented flyway and created the databases and added a mock for cambioController for testing

---
+++ 08_CreatingAndConnectingMicroservicesWithFeign/cambio-service/src/main/kotlin/com/guilherme/controller/CambioController.kt
@@ -0,0 +1,36 @@
+package com.guilherme.controller
+
+import com.guilherme.model.Cambio
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.core.env.Environment
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.math.BigDecimal
+
+@RestController
+@RequestMapping("cambio-service")
+class CambioController {
+    @Autowired
+    private lateinit var environment: Environment
+    @GetMapping(value = ["/{amount}/{from}/{to}"])
+    fun getCambio(
+        @PathVariable("amount") amount: BigDecimal,
+        @PathVariable("from") from: String,
+        @PathVariable("to") to: String,
+    ): Cambio {
+        val port = environment.getProperty("local.server.port")
+
+        return Cambio(
+            id = 1L,
+            from = from,
+            to = to,
+            conversionFactor = BigDecimal.ONE,
+            convertedValue = BigDecimal.ONE,
+            environment = "PORT $port"
+
+        )
+
+    }
+}
---
+++ 08_CreatingAndConnectingMicroservicesWithFeign/cambio-service/src/main/kotlin/com/guilherme/model/Cambio.kt
@@ -0,0 +1,13 @@
+package com.guilherme.model
+
+import java.math.BigDecimal
+
+data class Cambio (
+    var id:Long = 0,
+    var from:String = "",
+    var to:String = "",
+    var conversionFactor:BigDecimal = BigDecimal.ZERO,
+    var convertedValue:BigDecimal = BigDecimal.ZERO,
+    var environment:String = ""
+
+)
