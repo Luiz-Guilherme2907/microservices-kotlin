a01f9a44882bcc652b4e2057861de9b7815d5a73
Luiz-Guilherme2907 <luizguilhermemodler2907@gmail.com>
2024-03-07 18:00:45 -0300

    Consuming the Exchange Service Endpoint in Book Service

--- 08_CreatingAndConnectingMicroservicesWithFeign/book-service/src/main/kotlin/com/guilherme/controller/BookController.kt
+++ 08_CreatingAndConnectingMicroservicesWithFeign/book-service/src/main/kotlin/com/guilherme/controller/BookController.kt
@@ -2,14 +2,18 @@ package com.guilherme.controller
 
 import com.guilherme.model.Book
 import com.guilherme.repository.BookRepository
+import com.guilherme.response.Cambio
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.core.env.Environment
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.client.RestTemplate
+import org.springframework.web.client.getForEntity
 import java.lang.RuntimeException
 import java.util.*
+import kotlin.collections.HashMap
 
 @RestController
 @RequestMapping("book-service")
@@ -26,9 +30,23 @@ class BookController {
         @PathVariable("currency") currency: String
     ): Book?{
         val book = repository.findById(id).orElseThrow{RuntimeException("Book not Found")}
+
+        val params = HashMap<String, String>()
+        params["amount"] = book.price.toString()
+        params["from"] = "USD"
+        params["to"] = currency
+
+        val response = RestTemplate()
+            .getForEntity("http://localhost:8000/cambio-service/{amount}/{from}/{to}",
+                Cambio::class.java,
+                params
+                )
+        val cambio = response.body
+
         val port = environment.getProperty("local.server.port")
         book.environment = port
         book.currency = currency
+        book.price = cambio!!.convertedValue
         return book
     }
 }
---
+++ 08_CreatingAndConnectingMicroservicesWithFeign/book-service/src/main/kotlin/com/guilherme/response/Cambio.kt
@@ -0,0 +1,12 @@
+package com.guilherme.response
+
+data class Cambio (
+
+    var id:Long = 0,
+    var from:String = "",
+    var to:String = "",
+    var conversionFactor:Double,
+    var convertedValue:Double,
+    var environment:String? = ""
+
+)
